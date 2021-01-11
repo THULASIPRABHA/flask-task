388bcbd4ff3ff001ef64912dce80b3da210d106a
THULASIPRABHA <69714505+THULASIPRABHA@users.noreply.github.com>
2021-01-11 22:55:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from flask import *
-import re
-#using regex for multidelimiter split()
-
 app = Flask(__name__)
-
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/",methods=['GET', 'POST'])
+@app.route("/",methods=['GET','POST'])
 def func():
     if request.method == 'POST':
         text=request.form["userinput"]
         if request.form.get("WordCount"):
             output=len(text.split())
         elif request.form.get("title1"):
-            output=text.title()
+            lst = [word[0].upper() + word[1:] for word in text.split()]
+            text = " ".join(lst)
+            output=text
         elif request.form.get("unique"):
-            output=",".join(set(re.split('[,\s]\s*',text.lower())))
+
+           list_str = text.lower().split()
+           unique_words = []
+           for word in list_str:
+               if word not in unique_words:
+                   unique_words.append(word)
+            output=unique_words
+        
     return render_template("index.html", output=output)
+
+            
+    
 
 
 if __name__=="__main__":
